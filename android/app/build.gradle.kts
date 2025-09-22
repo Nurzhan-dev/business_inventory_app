37a1a8d634d66e72f541bb07279f81d7e08e6d54
Nurzhan-dev <musabaevnurzhan@gmail.com>
2025-09-23 02:27:54 +0500

    Update and rename build.gradle to build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.business_inventory_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.business_inventory_app"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = flutterVersionCode.toInt()
+        versionName = flutterVersionName
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
