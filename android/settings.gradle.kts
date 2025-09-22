a41bc782c53b66f243bb50b01a618dd938652826
Nurzhan-dev <musabaevnurzhan@gmail.com>
2025-09-23 02:39:13 +0500

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    includeBuild("/opt/hostedtoolcache/flutter/stable-3.35.2-x64/packages/flutter_tools/gradle")
 }
 
 include(":app")
