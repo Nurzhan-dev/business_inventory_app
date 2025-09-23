b648bbd924d673daf9f21e7354a71c637fc99a50
Nurzhan-dev <musabaevnurzhan@gmail.com>
2025-09-23 15:30:19 +0500

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,3 +7,15 @@ pluginManagement {
 }
 
 include(":app")
+
+// Подключаем Flutter SDK для плагина
+val localProperties = java.util.Properties()
+val localPropertiesFile = File(rootDir, "local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.reader(Charsets.UTF_8).use { localProperties.load(it) }
+}
+
+val flutterSdkPath = localProperties.getProperty("flutter.sdk")
+if (flutterSdkPath != null) {
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+}
